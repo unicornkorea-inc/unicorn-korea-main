bf5f9d93dbc5c35d587d3ebefa69a74331d95f55
unicornkorea-inc <nighta78@gmail.com>
2023-09-29 19:15:36 +0900

    성공사례 부분 스타일 수정

--- src/components/News/index.tsx
+++ src/components/News/index.tsx
@@ -1,24 +1,25 @@
 import Slider from "react-slick";
 import { useRecoilValue } from "recoil";
 import { successStoriesState } from "../../store/atoms/successStoriesState";
+import SuccessStory from "../Main/SuccessStory";
 import { NewsSection } from "./NewsSection";
 import { PolicyNews } from "./PolicyNews";
 import "./news.scss";
-import SuccessStory from "../Main/SuccessStory";
 
 export const News = () => {
   const successStories = useRecoilValue(successStoriesState);
   const settings = {
     dots: false,
     infinite: false,
-    // slidesToShow: 1,
+    slidesToShow: 2,
     slidesToScroll: 1,
-    style: { width: "100%" }
+    style: { width: "100%" },
+    arrows: false
   };
   return (
     <section className="news">
       <p className="section-title">유니콘코리아 성공사례</p>
-      <Slider {...settings}>
+      <Slider {...settings} className="desktop-success-stories">
         {successStories.map(
           ({ img, name, tags, representative, subtitle, content }) => (
             <SuccessStory
@@ -33,9 +34,22 @@ export const News = () => {
           )
         )}
       </Slider>
-
-      <section className="success">
-      </section>
+      <div className="mobile-success-stories">
+        {successStories.map(
+          ({ img, name, tags, representative, subtitle, content }) => (
+            <SuccessStory
+              img={img}
+              name={name}
+              tags={tags}
+              representative={representative}
+              subtitle={subtitle}
+              content={content}
+              hasButton={false}
+            />
+          )
+        )}
+      </div>
+      <section className="success"></section>
       <p className="section-title news-title">유니콘코리아 뉴스</p>
       <NewsSection />
       <p className="section-title news-title policy-news">정책 뉴스</p>
